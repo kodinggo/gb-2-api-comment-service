2ce182c5755c5f1250f2f42c627751b773794b70
CelticAlreadyUse <wahyusantosokanisius@gmail.com>
2024-11-06 20:36:01 +0700

    init console and config

---
+++ internal/console/serve.go
@@ -0,0 +1,26 @@
+package console
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "serve is a command to run the service server",
+	Run: func(cmd *cobra.Command, args []string) {
+		e := echo.New()
+
+		e.GET("/ping", func(c echo.Context) error {
+			return c.String(http.StatusOK, "pong!")
+		})
+
+		e.Start(":8091")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+}
